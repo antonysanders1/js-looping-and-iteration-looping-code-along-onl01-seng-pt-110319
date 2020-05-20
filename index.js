b48c5e4969d584f24dbe5ca010fbabdf0eeeae00
Antony Sanders <antony.devon92@gmail.com>
2020-05-20 06:22:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
-function writeCards(names, b) {
+function writeCards(names, sup) {
     let thankYouCards = []
     for (let i = 0; i < names.length; i++) {
-        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${b} gift!`)
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${sup} gift!`)
     }
     return thankYouCards
 }
