b4bfddc04ca1baaff08ec8015ac2d26ac3cb232c
Antony Sanders <antony.devon92@gmail.com>
2020-05-20 06:16:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function writeCards(names, b) {
     console.log(`Thank you, ${names[i]}, for the wonderful ${b} gift!`);
   }
  
-  return 
+  return `Thank you, ${names[i]}, for the wonderful ${b} gift!`);
 }
  
 writeCards(names, b);
