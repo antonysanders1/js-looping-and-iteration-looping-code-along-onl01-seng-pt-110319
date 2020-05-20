3a5acf80e9e749568573b141f55623be4ef47b49
Antony Sanders <antony.devon92@gmail.com>
2020-05-20 06:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-function writeCards(names, event) {
+function writeCards(names, b) {
     let thankYouCards = []
     for (let i = 0; i < names.length; i++) {
         thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
